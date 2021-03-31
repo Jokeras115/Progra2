77916a1d9b9c91a57918a8ccf8e944556ee742c8
camila <camila.liendro@gmail.com>
2021-03-30 21:46:06 -0300

    menu

---
+++ Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Menu/Scripts/Quality.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quality : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public int calidad;
+    void Start()
+    {
+        calidad = PlayerPrefs.GetInt("numeroDeCalidad", 3);
+        dropdown.value = calidad;
+        AjustarCalidad();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void AjustarCalidad()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value);
+        PlayerPrefs.SetInt("numeroDeCalidad", dropdown.value);
+        calidad = dropdown.value;
+    }
+}
