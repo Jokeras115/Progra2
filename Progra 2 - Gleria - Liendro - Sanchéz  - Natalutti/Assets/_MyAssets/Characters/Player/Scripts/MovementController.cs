80b3060465f1004321dc6472f246f1d88426b610
TheJokeras115 <73303357+Jokeras115@users.noreply.github.com>
2021-05-06 16:32:28 -0300

    Character Asset and Animations

---
+++ Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Characters/Player/Scripts/MovementController.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    #region Private Variables
+    [Header("References")]
+    [Space]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    private PlayerController player;
+    private KeyCode lastKeyCode;
+
+    [Header("Private Flaots")]
+    [Space]
+
+    private float speed = 15f;
+    private float jumpForce = 15f;
+    private const float groundcheckRadius = 0.2f;
+    private float xMove;
+    private float doubleTapTime;
+
+    [Header("Public Floats")]
+    [Space]
+
+    public float dashDistance = 15f;
+
+    [Header("Private Bools")]
+    [Space]
+
+    private bool facingRight;
+
+    [Header("Public Bools")]
+    [Space]
+
+    public bool isGrounded;
+    public bool canDoubleJump;
+    public bool isDashing;
+    #endregion
+
+    #region Public Variables
+    #endregion
+
+    #region Unity Methods
+    void Awake()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Inputs();
+    }
+
+    private void FixedUpdate()
+    {
+        BasicMovement();
+        GroundCheck();
+    }
+    #endregion
+
+    #region Public Functions
+    #endregion
+
+    #region Private Functions
+    private void Inputs()
+    {
+        //Move Input
+        xMove = Input.GetAxisRaw("Horizontal");
+
+        //Jump Input (Spacebar)
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        //Dash Input (double tap A or D)
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DashL();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DashR();
+        }
+    }
+
+    private void BasicMovement()
+    {
+        #region Movement Logic
+        player.rb.velocity = new Vector2(xMove * speed, player.rb.velocity.y);
+        #endregion
+
+        #region Flip
+        if (facingRight && xMove < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            facingRight = false;
+        }
+        else if (!facingRight && xMove > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight = true;
+        }
+        #endregion
+    }
+
+    private void Jump()
+    {
+        if (isGrounded == true)
+        {
+            player.rb.velocity = Vector2.up * jumpForce;
+            canDoubleJump = true;
+        }
+        else if (canDoubleJump == true)
+        {
+            player.rb.velocity = new Vector2(player.rb.velocity.x, jumpForce);
+            canDoubleJump = false;
+        }
+    }
+
+    private void DashL()
+    {
+        //Dash Left
+        if (doubleTapTime > Time.time && lastKeyCode == KeyCode.A)
+        {
+            StartCoroutine(Dashing(-1f));
+        }
+        else
+        {
+            doubleTapTime = Time.time + 0.5f;
+        }
+
+        lastKeyCode = KeyCode.A;
+    }
+
+    private void DashR()
+    {
+        //Dash Right
+        if (doubleTapTime > Time.time && lastKeyCode == KeyCode.D)
+        {
+            StartCoroutine(Dashing(1f));
+        }
+        else
+        {
+            doubleTapTime = Time.time + 0.5f;
+        }
+
+        lastKeyCode = KeyCode.D;
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundcheckRadius, groundLayer);
+        if (colliders.Length > 0)
+        {
+            isGrounded = true;
+        }
+    }
+
+    //Dash Logic (execute in many frames)
+    IEnumerator Dashing(float direction)
+    {
+        isDashing = true;
+        player.rb.velocity = new Vector2(player.rb.velocity.x, 0f);
+        player.rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+        float gravity = player.rb.gravityScale;
+        player.rb.gravityScale = 0;
+        yield return new WaitForSeconds(0.4f);
+        isDashing = false;
+        player.rb.gravityScale = gravity;
+    }
+    #endregion
+}
--- Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Characters/Player/Scripts/PlayerController.cs
+++ Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Characters/Player/Scripts/PlayerController.cs
@@ -4,180 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Variables
-    //SerializeField - Private - Public (In that order)
-
-    [Header("References")]
-    [Space]
-    //[SerializeField] private Animator anim;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundLayer;
-    private KeyCode lastKeyCode;
-    public Rigidbody2D rb;
-
-    [Header("Floats")]
-    [Space]
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpForce = 5f;
-    private const float groundcheckRadius = 0.2f;
-    private float xMove;
-    private float doubleTapTime;
-    public float dashDistance = 15f;
-     
-    [Header("Integers")]
+    #region Private Variables
+    [Header("Privates References")]
     [Space]
 
-    [Header("Bools")]
-    [Space]
-    private bool facingRight;
-    public bool isGrounded;
-    public bool canDoubleJump;
-    public bool isDashing;
+    private MovementController movement;
     #endregion
 
-    #region BaseFunctions
-    private void Awake()
+    #region Public Variables
+    [Header("Public References")]
+    [Space]
+    public Rigidbody2D rb;
+    #endregion
+
+    #region Unity Methods
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
     }
 
-    private void Update()
+    void Start()
     {
-        Inputs();
+        
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        BasicMovement();
-        GroundCheck();
+        
     }
     #endregion
 
-    #region MyFunctions
-
-    #region Private
-    private void Inputs()
-    {
-        //Move Input
-        xMove = Input.GetAxisRaw("Horizontal");
-
-        //Jump Input (Spacebar)
-        if (Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
-
-        //Dash Input (double tap A or D)
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            DashL();
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            DashR();
-        }
-    }
-
-    private void BasicMovement()
-    {
-        #region Move
-        if (!isDashing)
-        {
-            rb.velocity = new Vector2(xMove * speed, rb.velocity.y);
-        }
-        #endregion
-
-        #region Flip
-        if (facingRight && xMove < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-            facingRight = false;
-        }
-        else if (!facingRight && xMove > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-            facingRight = true;
-        }
-        #endregion
-
-    }
-
-    private void Jump()
-    {
-        if (isGrounded == true)
-        {
-            rb.velocity = Vector2.up * jumpForce;
-            canDoubleJump = true;
-        }
-        else if(canDoubleJump == true)
-        {
-            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
-            canDoubleJump = false;
-        }
-    }
-
-    private void DashL()
-    {
-        //Dash Left
-        if (doubleTapTime > Time.time && lastKeyCode == KeyCode.A)
-        {
-            StartCoroutine(Dashing(-1f));
-        }
-        else
-        {
-            doubleTapTime = Time.time + 0.5f;
-        }
-
-        lastKeyCode = KeyCode.A;
-    }
-
-    private void DashR()
-    {
-        //Dash Right
-        if (doubleTapTime > Time.time && lastKeyCode == KeyCode.D)
-        {
-            StartCoroutine(Dashing(1f));
-        }
-        else
-        {
-            doubleTapTime = Time.time + 0.5f;
-        }
-
-        lastKeyCode = KeyCode.D;
-    }
-
-    private void GroundCheck()
-    {
-        isGrounded = false;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundcheckRadius, groundLayer);
-        if (colliders.Length > 0)
-        {
-            isGrounded = true;
-        }         
-    }
-
-    //Dash Logic (execute in many frames)
-    IEnumerator Dashing(float direction)
-    {
-        isDashing = true;
-        rb.velocity = new Vector2(rb.velocity.x, 0f);
-        rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
-        float gravity = rb.gravityScale;
-        rb.gravityScale = 0;
-        yield return new WaitForSeconds(0.4f);
-        isDashing = false;
-        rb.gravityScale = gravity;
-    }
+    #region Public Functions
     #endregion
 
-    #region Public
-    public void TakeDamage() 
-    {
-
-    }
-    #endregion
-
+    #region Private Functions
     #endregion
 }
