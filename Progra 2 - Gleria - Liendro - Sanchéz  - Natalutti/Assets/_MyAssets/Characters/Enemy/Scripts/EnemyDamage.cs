747543ed0c60e63b61f533a49e9c497cc389e8cb
camila <camila.liendro@gmail.com>
2021-07-13 23:50:06 -0300

    Enemies add
    Enemies added

--- Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Characters/Enemy/Scripts/EnemyDamage.cs
+++ Progra 2 - Gleria - Liendro - Sanchéz  - Natalutti/Assets/_MyAssets/Characters/Enemy/Scripts/EnemyDamage.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
-{
-    public PlayerHealth PlayerHealth;
+{  
+    public PlayerHealth PlayerHealth;   
     public int damage = 5;
+    public static bool Attack = false;
+    Animator anim;
+    
 
+   
     private void OnCollisionEnter2D(Collision2D coll)
     {
         PlayerHealth player = coll.gameObject.GetComponent<PlayerHealth>();
         if (player != null)
         {
             player.TakeDamage(damage);
+           
         }
     }
 }
